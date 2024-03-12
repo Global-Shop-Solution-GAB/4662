05074e6a214147f5708421b74ada911463bf43ac
Service Web <sw@gssmail.com>
2024-03-12 12:31:51 -0500

    Commit changes in C:\Websites\Git\emarlow\4662\GCG_4662_DATA.sql

---
+++ GCG_4662_DATA.sql
@@ -0,0 +1,16 @@
+/* ==========================================
+ * TABLE: GCG_4662_DATA
+ * ========================================== */
+CREATE TABLE "GCG_4662_DATA"(
+ "JOB" CHAR(6),
+ "SUFFIX" CHAR(3),
+ "SEQ" CHAR(6),
+ "DEPARTMENT" LONGVARCHAR,
+ "WORK_CENTER" LONGVARCHAR,
+ "DISCREPANCY" LONGVARCHAR,
+ "SCRAP_CODE" LONGVARCHAR,
+ "VENDOR" LONGVARCHAR,
+ "ACT_TYPE" CHAR(1),
+ "DATE_CLOSED" DATE);
+
+
